833709a159b387cde63ff5e5fd348f4046e311cf
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2016-06-16 17:19:00 +0200

    Update documentation

--- R/Fire.R
+++ R/Fire.R
@@ -36,8 +36,8 @@ NULL
 #' }
 #' 
 #' @importFrom R6 R6Class
-#' @importFrom assertthat is.string is.count is.number has_args assert_that is.dir
-#' @importFrom httpuv startServer service startDaemonizedServer stopDaemonizedServer
+#' @importFrom assertthat is.string is.count is.number has_args assert_that is.dir is.flag
+#' @importFrom httpuv startServer service startDaemonizedServer stopDaemonizedServer stopServer
 #' @importFrom uuid UUIDgenerate
 #' @importFrom utils browseURL
 #' 
@@ -47,6 +47,57 @@ NULL
 #' @examples 
 #' # Create a New App
 #' app <- Fire$new()
+#' app$port <- 4689
+#' 
+#' # Setup the data everytime it starts
+#' app$on('start', function(server, ...) {
+#'     server$set_data('visits', 0)
+#'     server$set_data('cycles', 0)
+#' })
+#' 
+#' # Count the number of cycles
+#' app$on('cycle-start', function(server, ...) {
+#'     server$set_data('cycles', server$get_data('cycles') + 1)
+#' })
+#' 
+#' # Count the number of requests
+#' app$on('before-request', function(server, ...) {
+#'     server$set_data('visits', server$get_data('visits') + 1)
+#' })
+#' 
+#' # Handle requests
+#' app$on('request', function(server, ...) {
+#'     list(
+#'         status = 200L,
+#'         headers = list('Content-Type' = 'text/html'),
+#'         body = paste('This is indeed a test. You are number', server$get_data('visits'))
+#'     )
+#' })
+#' 
+#' # Show number of requests in the console
+#' app$on('after-request', function(server, ...) {
+#'     message(server$get_data('visits'))
+#'     flush.console()
+#' })
+#' 
+#' # Terminate the server after 300 cycles
+#' app$on('cycle-end', function(server, ...) {
+#'     if (server$get_data('cycles') > 300) {
+#'         message('Ending...')
+#'         flush.console()
+#'         server$extinguish()
+#'     }
+#' })
+#' 
+#' # Be polite
+#' app$on('end', function(server) {
+#'     message('Goodbye')
+#'     flush.console()
+#' })
+#' 
+#' \dontrun{
+#' app$ignite(showcase = TRUE)
+#' }
 #' 
 Fire <- R6Class('Fire',
     public = list(
