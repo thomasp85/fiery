d537b9e9a7b75db38b8b1660a09fdaf35dcbd12f
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2016-07-02 23:12:47 +0200

    Testing TimeStack

--- tests/testthat/test-FutureStack.R
+++ tests/testthat/test-FutureStack.R
@@ -26,3 +26,57 @@ test_that('DelayStack works', {
     delay$remove(id)
     expect_true(delay$empty())
 })
+
+test_that('TimeStack works', {
+    catcher <- new.env()
+    time <- TimeStack$new()
+    expect_true(time$empty())
+    time$add({
+        message('test')
+        10
+    }, function(res, ...) {
+        catcher$res <- res + 10
+    }, after = 2)
+    expect_false(time$empty())
+    expect_silent(time$eval())
+    Sys.sleep(3)
+    expect_message(time$eval(), 'test')
+    expect_equal(catcher$res, 20)
+    expect_true(time$empty())
+    
+    time$add({
+        message('test')
+        10
+    }, function(res, ...) {
+        catcher$res <- res + 20
+    }, after = 2)
+    Sys.sleep(3)
+    time$reset()
+    expect_silent(time$eval())
+    Sys.sleep(3)
+    expect_message(time$eval(), 'test')
+    expect_equal(catcher$res, 30)
+    expect_true(time$empty())
+    
+    id <- time$add({
+        message('test')
+        10
+    }, function(res, ...) {
+        catcher$res <- res + 30
+    }, after = 2, loop = TRUE)
+    expect_silent(time$eval())
+    Sys.sleep(3)
+    expect_message(time$eval(), 'test')
+    expect_false(time$empty())
+    expect_equal(catcher$res, 40)
+    expect_silent(time$eval())
+    Sys.sleep(3)
+    expect_message(time$eval(), 'test')
+    time$remove(id)
+    
+    expect_true(time$empty())
+    id <- time$add(10, after = 1)
+    expect_false(time$empty())
+    time$remove(id)
+    expect_true(time$empty())
+})
